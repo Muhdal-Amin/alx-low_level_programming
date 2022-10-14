96f040338f5e63f9455b1cfa3fcf411bb387a79f
Muhdal-Amin <adalemo2000@gmail.com>
2022-10-13 18:12:24 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+
+/**
+*main-"Print"
+*
+*followed by a new line
+*Return:"Always 0"
+**/
 int main(void)
 
 {
-  puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 
 }
