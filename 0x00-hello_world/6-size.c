37fe1ea33108e0e9a78dcbb514581f90c4c0f4aa
AL 'AMIN <113265168+Muhdal-Amin@users.noreply.github.com>
2022-10-16 22:06:35 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main-"print"
-*
-* followed by a new line
-* Return:"Always (0)"
-**/
+ * main - Code entry point
+ *
+ * followed by a new line
+ * Return:"Always (0)"
+*/
 
 
 int main(void)
