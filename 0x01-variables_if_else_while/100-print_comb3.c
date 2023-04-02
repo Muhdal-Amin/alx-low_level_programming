3641773fcb335f4eff03ffeef76a048b30e58f6a
Muhdal-Amin <adalemo2000@gmail.com>
2023-04-02 05:58:32 -0700

    Update - File

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,24 +12,24 @@
 
 int main(void)	
 {	
-int comb1, comb2;	
-for (comb1 = '0'; comb2 <= '9'; comb1++)
-{
-for (comb2 = comb1 + 1; comb2 <= '9'; comb2++)
-{
-if (comb1 != comb2)
-{
-putchar(comb1);
-putchar(comb2);
-if (comb1 == '8' && comb2 == '9')
-continue;
-putchar(',');
-putchar(' ')
-}
-}
-}	
-putchar('\n');
-return (0);
+	int comb1, comb2;	
+	for (comb1 = '0'; comb2 <= '9'; comb1++)
+	{
+		for (comb2 = comb1 + 1; comb2 <= '9'; comb2++)
+		{
+			if (comb1 != comb2)
+			{
+				putchar(comb1);
+				putchar(comb2);
+				if (comb1 == '8' && comb2 == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+					}
+		}
+	}	
+	putchar('\n');
+	return (0);
 }
 
 
