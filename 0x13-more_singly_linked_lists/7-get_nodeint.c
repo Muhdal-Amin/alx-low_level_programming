0903fc30305c561922a99a74c16e20a3545fa1ff
Muhdal-Amin <adalemo2000@gmail.com>
2023-05-02 14:02:26 -0700

    Update - File

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,9 +12,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (i = 0; i < index && head != NULL; i++)
+	for (i = 0; i < index; i++)
 	{
+		if (head != NULL)
+		{
 		head = head->next;
+		}
+
+		else
+		{
+			return (NULL);
+		}
 	}
 
 	return (head);
