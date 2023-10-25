4a9d31503c2d372252665cfc42715e29742f7d84
Muhdal-Amin <adalemo2000@gmail.com>
2023-10-25 14:01:42 -0700

    Task Completed

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Module that contains the island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Args:
+    grid (List[List[int]]): A grid where 1 rep land and 0 rep water.
+
+    Returns:
+    int: The perimeter of the island.
+
+    """
+
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
