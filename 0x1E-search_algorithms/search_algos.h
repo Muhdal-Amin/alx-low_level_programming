78bcf1c80142d2727d6ffe6a0ff08a3a960e619a
Muhdal-Amin <adalemo2000@gmail.com>
2024-01-15 23:26:07 +0000

    Updated C header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,22 +5,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * Descripton: singly linked list node structure
- */
-
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-
-} listint_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
